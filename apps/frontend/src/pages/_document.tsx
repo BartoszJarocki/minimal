8f2ac0b13a46164dff03659ce1dc3ae214edaee6
Bartosz Jarocki <jarocki.bartek@gmail.com>
2023-01-26 14:55:25 +0100

    A5 - WIP

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -24,7 +24,7 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
