73cb3022802f03eaeb617ea4a206c1b4c6fe5168
Bartosz Jarocki <jarocki.bartek@gmail.com>
2023-03-04 16:34:11 +0100

    Code cleanup

--- apps/frontend/src/components/calendar/themes/SimpleMinimalist.tsx
+++ apps/frontend/src/components/calendar/themes/SimpleMinimalist.tsx
@@ -32,7 +32,7 @@ export const SimpleMilimalistYearCalendar = ({
         yearHeader:
           "text-vertical-rl mb-8 ml-auto basis-[27%] translate-x-14 rotate-180 self-end text-end text-[144px] font-semibold leading-none tracking-tighter",
         monthHeader:
-          "flex items-center px-1 text-2xl font-semibold leading-tight tracking-tighter",
+          "flex items-center px-1 text-2xl font-semibold leading-none tracking-tighter",
         monthsGrid: "grid grid-cols-2 gap-4",
         dayCell: "flex items-center justify-center text-center text-[12px]",
       },
